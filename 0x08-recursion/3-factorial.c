e97b45ac6a915b9727050008e2eaa300fa0b62d6
Blackmask001 <Dakings88@gmail.com>
2022-09-29 15:11:03 -0700

    recursion tasks

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,28 +1,16 @@
-
 /**
- * _pow_recursion - a function that returns the power of @x by @y x^y
+ * factorial - a function that finds the factorial of *n
  *
- * Description: this method is good for speed, check this youtube video
- *   https://youtu.be/wAyrtLAeWvI?list=PL2_aWCzGMAwLz3g66WrxFGSXvSsvyfzCO
+ * @n: positive number
  *
- * @x: integer x
- * @y: integer y
- *
- * Return: the power of @x by @y
+ * Return: factorial of @n
 */
-int _pow_recursion(int x, int y)
+int factorial(int n)
 {
-	int even;
-
-	if (y < 0)		/*if y is a negative number, return -1*/
+	if (n < 0)		/*error -1 if number is negative*/
 		return (-1);
-	else if (y == 0)	/*x to the power of 0 is 1*/
+	else if (n == 0)	/*return factorial of 0 = 1*/
 		return (1);
-	else if (y % 2 == 0)	/*if y is an even number*/
-	{
-		even = _pow_recursion(x, y / 2);
-		return (even * even);
-	}
-	else			/*if y is odd*/
-		return (x * _pow_recursion(x, y - 1));
+	else			/*do factorial of n if its not less or equal to 0*/
+		return (n * factorial(n - 1));
 }
