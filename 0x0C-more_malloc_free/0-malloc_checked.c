a8f40033dc1183ac320481556fa54a0c6184942a
Blackmask001 <Dakings88@gmail.com>
2022-10-13 21:45:53 -0700

    nna eh

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+
 /**
  * malloc_checked - allocates memory using malloc
  *
@@ -9,11 +10,15 @@
 */
 
 void *malloc_checked(unsigned int b)
+
 {
 	void *new_mem;
+
 	new_mem = malloc(b);
+
 	if (new_mem == NULL)
 		exit(98);
+
 	return (new_mem);
 }
 
